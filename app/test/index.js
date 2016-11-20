17af5611d6fed726af6e6f72f58117a58ee52b69
enunez_91 <eduardodaniel.03@gmail.com>
2016-11-20 17:26:51 -0400

    Add unit test category api

---
+++ app/test/index.js
@@ -0,0 +1,37 @@
+var test = require('assert');
+var superagent = require('superagent');
+var express = require('express');
+var httpStatus = require('http-status');
+var wagner = require('wagner-core');
+
+var connection = require('../settings/connectionString');
+
+describe('Category API',function(){
+  var server;
+
+  before(function(){
+    var app = express();
+
+    require('../models/category')(wagner);
+
+    app.use(require('../api/category')(wagner));
+
+    server = app.listen(connection.SERVER_PORT);
+  });
+
+  after(function(){
+    server.close();
+  });
+
+  describe('Server', function(){
+    it('#request.get(\'/category\')',function(){
+      superagent.get('/category').end(function(error,res){
+          test.ifError(error);
+          test.equal(res.status,httpStatus.OK);
+          test.equal(res.text,'I am here!');
+          done();
+      });
+    });
+  });
+
+});
